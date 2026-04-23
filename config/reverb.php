a3683f115ae3cfbecf864d61dd2b574cae36e3dd
Mina Maher Wanis <minamaherwanis@gmail.com>
2026-04-23 12:12:57 +0200

    refactor: clean up Reverb configuration by removing unnecessary comments and TLS settings

--- config/reverb.php
+++ config/reverb.php
@@ -2,29 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Reverb Server
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default server used by Reverb to handle
-    | incoming messages as well as broadcasting message to all your
-    | connected clients. At this time only "reverb" is supported.
-    |
-    */
-
     'default' => env('REVERB_SERVER', 'reverb'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Reverb Servers
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define details for each of the supported Reverb servers.
-    | Each server has its own configuration options that are defined in
-    | the array below. You should ensure all the options are present.
-    |
-    */
 
     'servers' => [
 
@@ -32,15 +10,15 @@ return [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 8080),
             'path' => env('REVERB_SERVER_PATH', ''),
+
+            // مهم جدًا: ده اللي بيخلي Cloud يشتغل
             'hostname' => env('REVERB_HOST'),
-            'options' => [
-                'tls' => [
-                    'local_cert' => 'C:/laragon/etc/ssl/laragon.crt',
-                    'local_pk' => 'C:/laragon/etc/ssl/laragon.key',
-                    'verify_peer' => false,
-                ],
-            ],
+
+            // ❌ شيلنا TLS local نهائي
+            'options' => [],
+
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
+
             'scaling' => [
                 'enabled' => env('REVERB_SCALING_ENABLED', false),
                 'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
@@ -54,22 +32,12 @@ return [
                     'timeout' => env('REDIS_TIMEOUT', 60),
                 ],
             ],
+
             'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
         ],
 
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Reverb Applications
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define how Reverb applications are managed. If you choose
-    | to use the "config" provider, you may define an array of apps which
-    | your server will support, including their connection credentials.
-    |
-    */
 
     'apps' => [
 
@@ -80,13 +48,14 @@ return [
                 'key' => env('REVERB_APP_KEY'),
                 'secret' => env('REVERB_APP_SECRET'),
                 'app_id' => env('REVERB_APP_ID'),
+
                 'options' => [
                     'host' => env('REVERB_HOST'),
-                    'port' => env('REVERB_PORT', 8080),
+                    'port' => env('REVERB_PORT', 443), // 👈 خليها 443
                     'scheme' => env('REVERB_SCHEME', 'https'),
-                    'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
-                    'verify_peer' => false,
+                    'useTLS' => true,
                 ],
+
                 'allowed_origins' => ['*'],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
