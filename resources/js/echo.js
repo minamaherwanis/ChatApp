d57aa04f30b8cfc30fa5d7d5e93db3f81165e17b
Mina Maher Wanis <minamaherwanis@gmail.com>
2026-04-23 13:50:33 +0200

    refactor: simplify Echo configuration by consolidating transport settings

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,26 +1,14 @@
-import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
+import Echo from 'laravel-echo';
 
-window.Pusher = Pusher
-
-const reverbScheme = import.meta.env.VITE_REVERB_SCHEME ?? 'http'
-const isSecure = reverbScheme === 'https'
-const configuredHost = import.meta.env.VITE_REVERB_HOST
-const browserHost = window.location.hostname
-const localHosts = new Set(['localhost', '127.0.0.1'])
-
-const wsHost = localHosts.has(browserHost)
-    ? browserHost
-    : (configuredHost ?? browserHost)
-const wsPort = Number(import.meta.env.VITE_REVERB_PORT ?? (isSecure ? 443 : 80))
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost,
-    wsPort,
-    wssPort: wsPort,
-    forceTLS: isSecure,
-    disableStats: true,
-    enabledTransports: isSecure ? ['wss'] : ['ws'],
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
